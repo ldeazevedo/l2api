849e799b44287e7ea671a8bc8dae97dba627e7cd
Emmanuel Leonardo De Azevedo <emmanuelleonardo.deazevedo@web.com>
2020-06-04 10:34:19 +0200

    Mapping Player to APIPlayer

--- src/main/java/com/atiq/handler/player/PlayerInfoHandler.java
+++ src/main/java/com/atiq/handler/player/PlayerInfoHandler.java
@@ -1,5 +1,6 @@
 package com.atiq.handler.player;
 
+import com.atiq.model.APIPlayer;
 import net.sf.l2j.gameserver.model.World;
 
 import javax.servlet.ServletException;
@@ -14,7 +15,7 @@ public class PlayerInfoHandler extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String playerName = request.getParameter("playerName");
         if (playerName != null && !playerName.isEmpty())
-            request.setAttribute("player", World.getInstance().getPlayer(playerName));
+            request.setAttribute("player", new APIPlayer(World.getInstance().getPlayer(playerName)));
         request.getRequestDispatcher("/playerdata.jsp").forward(request, response);
     }
 }
